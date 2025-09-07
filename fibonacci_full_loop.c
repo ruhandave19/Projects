2b9bc585997e2b3d6db70cb767376ccad495ada2
Ruhan <ruhandave14@gmail.com>
2025-09-07 15:44:32 +0530

    First commit - uploaded C projects

---
+++ fibonacci_full_loop.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+// When the user enters 8, I need the computer to print 0, 1, 1, 2, 3, 5, 8, 13
+
+int main(){
+  int num;
+  int i = 0;    
+  int j = 1;
+  int x;
+  printf("Enter a number: ");
+  scanf("%d", &num);
+  if(num>2){
+    printf("The fibonacci series till position %d is ", num); 
+    printf("0, 1, ");
+    for(int a=1; a<=(num-2); a++){
+      x = i+j;
+      i = j;
+      j = x;
+      printf("%d, ", x);
+    }
+    printf("\b\b ");
+  }
+  else if(num==1){
+    printf("The fibonacci series till position 1 is 0");
+  }
+  else{
+    printf("The fibonacci series till position 2 is 0, 1");
+  }
+}
